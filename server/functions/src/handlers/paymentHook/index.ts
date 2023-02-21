eafe529c00fe50f767d7394bbb63cf9e48f1c1c9
michealnaita <michealnaita@gmail.com>
2023-02-21 12:28:07 +0300

    feat: uses env vars from secret manager

--- server/functions/src/handlers/paymentHook/index.ts
+++ server/functions/src/handlers/paymentHook/index.ts
@@ -7,69 +7,73 @@ import { Transaction, User } from '../../utils/types';
 process.env.NODE_ENV === 'testing' && admin.initializeApp();
 const db = admin.firestore();
 
-const paymentHook = functions.https.onRequest(async (req, res) => {
-  const { FLW_PUBLIC_KEY, FLW_SECRET_KEY, NODE_ENV, APP_URL } = process.env;
-  try {
-    const flw = new Flutterwave(
-      FLW_PUBLIC_KEY as string,
-      FLW_SECRET_KEY as string,
-      NODE_ENV === 'production'
-    );
-    const { tx_ref = 'null', transaction_id = 'null' } = req.query;
-    if (!(tx_ref && transaction_id))
-      throw new BadRequestError('missing-tx_ref-and-transaction_id');
+const paymentHook = functions
+  .runWith({
+    secrets: ['FLW_PUBLIC_KEY', 'FLW_SECRET_KEY', 'NODE_ENV', 'APP_URL'],
+  })
+  .https.onRequest(async (req, res) => {
+    const { FLW_PUBLIC_KEY, FLW_SECRET_KEY, NODE_ENV, APP_URL } = process.env;
+    try {
+      const flw = new Flutterwave(
+        FLW_PUBLIC_KEY as string,
+        FLW_SECRET_KEY as string,
+        NODE_ENV === 'production'
+      );
+      const { tx_ref = 'null', transaction_id = 'null' } = req.query;
+      if (!(tx_ref && transaction_id))
+        throw new BadRequestError('missing-tx_ref-and-transaction_id');
 
-    // Check if transaction ref exists
-    const txDoc = await db.doc('transactions/' + tx_ref).get();
-    if (!txDoc.exists) throw new BadRequestError('invalid-tx_ref');
-    const {
-      user_uid,
-      amount: tx_amount,
-      fulfilled,
-    } = txDoc.data() as Transaction;
-    if (fulfilled) throw new BadRequestError('transaction-already-fulfilled');
+      // Check if transaction ref exists
+      const txDoc = await db.doc('transactions/' + tx_ref).get();
+      if (!txDoc.exists) throw new BadRequestError('invalid-tx_ref');
+      const {
+        user_uid,
+        amount: tx_amount,
+        fulfilled,
+      } = txDoc.data() as Transaction;
+      if (fulfilled) throw new BadRequestError('transaction-already-fulfilled');
 
-    // check that transaction is valid
-    // TODO: REVERT THIS
-    // const tx = await Promise.resolve({
-    //   status: 'success',
-    //   data: { status: 'successful' },
-    // });
-    const { status, data } = await flw.Transaction.verify({
-      id: transaction_id,
-    });
-    if (status !== 'success')
-      return res.redirect(APP_URL + '/recharge?status=fail');
-
-    if (data.amount && status === 'success') {
+      // check that transaction is valid
       // TODO: REVERT THIS
-      // const { amount } = await Promise.resolve({ amount: 5000 });
-      if (!(data.amount == tx_amount))
-        throw new InternalServerError('different-transaction-amounts');
-      const userDoc = await db.doc('users/' + user_uid).get();
-      const { current_amount } = userDoc.data() as User;
-      const newAmount =
-        parseFloat(current_amount as any) + parseFloat(data.amount as any);
-      await db.doc('users/' + user_uid).update({
-        current_amount: newAmount,
+      // const tx = await Promise.resolve({
+      //   status: 'success',
+      //   data: { status: 'successful' },
+      // });
+      const { status, data } = await flw.Transaction.verify({
+        id: transaction_id,
       });
-      await db.doc('transactions/' + tx_ref).update({
-        fulfilled: true,
-      });
-      return res.redirect(APP_URL + '/recharge?status=success');
-    }
-    throw new InternalServerError('transaction-neither-fail-nor-success');
-  } catch (e: any) {
-    const s = new URLSearchParams();
-    e.code && s.set('code', e.code);
-    e.message && s.set('message', e.message);
-    const q = s.toString();
-    if (e instanceof BadRequestError)
-      return res.redirect(APP_URL + '/400?' + q);
-    return res.redirect(APP_URL + '/500?' + q);
-  }
+      if (status !== 'success')
+        return res.redirect(APP_URL + '/recharge?status=fail');
 
-  //
-});
+      if (data.amount && status === 'success') {
+        // TODO: REVERT THIS
+        // const { amount } = await Promise.resolve({ amount: 5000 });
+        if (!(data.amount == tx_amount))
+          throw new InternalServerError('different-transaction-amounts');
+        const userDoc = await db.doc('users/' + user_uid).get();
+        const { current_amount } = userDoc.data() as User;
+        const newAmount =
+          parseFloat(current_amount as any) + parseFloat(data.amount as any);
+        await db.doc('users/' + user_uid).update({
+          current_amount: newAmount,
+        });
+        await db.doc('transactions/' + tx_ref).update({
+          fulfilled: true,
+        });
+        return res.redirect(APP_URL + '/recharge?status=success');
+      }
+      throw new InternalServerError('transaction-neither-fail-nor-success');
+    } catch (e: any) {
+      const s = new URLSearchParams();
+      e.code && s.set('code', e.code);
+      e.message && s.set('message', e.message);
+      const q = s.toString();
+      if (e instanceof BadRequestError)
+        return res.redirect(APP_URL + '/400?' + q);
+      return res.redirect(APP_URL + '/500?' + q);
+    }
+
+    //
+  });
 
 export default paymentHook;
--- server/functions/src/handlers/rechargeLink/index.ts
+++ server/functions/src/handlers/rechargeLink/index.ts
@@ -10,71 +10,73 @@ import { v4 } from 'uuid';
 process.env.NODE_ENV === 'testing' && admin.initializeApp();
 const db = admin.firestore();
 
-const generateRechargeLink = functions.https.onCall(
-  async ({ phone, amount, email, name }: PaymentRequestData, context) => {
-    const { APP_URL, PAYMENT_WEBHOOK, NODE_ENV } = process.env;
-    // Check for user auth
-    if (context.auth) {
-      if (!context.auth.uid)
+const generateRechargeLink = functions
+  .runWith({ secrets: ['APP_URL', 'PAYMENT_WEBHOOK', 'NODE_ENV'] })
+  .https.onCall(
+    async ({ phone, amount, email, name }: PaymentRequestData, context) => {
+      const { APP_URL, PAYMENT_WEBHOOK, NODE_ENV } = process.env;
+      // Check for user auth
+      if (context.auth) {
+        if (!context.auth.uid)
+          throw new functions.https.HttpsError(
+            'unauthenticated',
+            'Did not receive user UID'
+          );
+      } else {
         throw new functions.https.HttpsError(
           'unauthenticated',
-          'Did not receive user UID'
+          'provide user auth object'
         );
-    } else {
-      throw new functions.https.HttpsError(
-        'unauthenticated',
-        'provide user auth object'
-      );
-    }
-    if (!(phone && amount && email && name))
-      throw new functions.https.HttpsError(
-        'failed-precondition',
-        'provide all user details'
-      );
-    const tx_ref = NODE_ENV === 'testing' ? 'tx_ref' : v4();
-    const config = {
-      headers: {
-        Authorization: `Bearer ${process.env.FLW_SECRET_KEY}`,
-      },
-    };
-    const payload = {
-      tx_ref,
-      amount,
-      currency: 'UGX',
-      redirect_url: PAYMENT_WEBHOOK,
-      customer: {
-        email,
-        phonenumber: '256' + phone,
-        name,
-      },
-      customizations: {
-        title: 'Kanji App',
-        logo: APP_URL + '/images/logo.svg',
-      },
-    };
-    try {
-      const { data }: { data: FlutterwaveResponse } = await axios.post(
-        process.env.FLUTTERWAVE_URL as string,
-        payload,
-        config
-      );
-      if (data.status !== 'success')
+      }
+      if (!(phone && amount && email && name))
         throw new functions.https.HttpsError(
-          'cancelled',
-          'couldnt generate recharge link'
+          'failed-precondition',
+          'provide all user details'
         );
-      const txData: Transaction = {
-        user_uid: context.auth.uid,
-        amount: parseInt(amount as any),
-        fulfilled: false,
+      const tx_ref = NODE_ENV === 'testing' ? 'tx_ref' : v4();
+      const config = {
+        headers: {
+          Authorization: `Bearer ${process.env.FLW_SECRET_KEY}`,
+        },
       };
-      await db.doc('transactions/' + tx_ref).create(txData);
-      return data;
-    } catch (e: any) {
-      if (e! instanceof functions.https.HttpsError)
-        throw new functions.https.HttpsError('unknown', e.message);
-      throw e;
+      const payload = {
+        tx_ref,
+        amount,
+        currency: 'UGX',
+        redirect_url: PAYMENT_WEBHOOK,
+        customer: {
+          email,
+          phonenumber: '256' + phone,
+          name,
+        },
+        customizations: {
+          title: 'Kanji App',
+          logo: APP_URL + '/images/logo.svg',
+        },
+      };
+      try {
+        const { data }: { data: FlutterwaveResponse } = await axios.post(
+          process.env.FLUTTERWAVE_URL as string,
+          payload,
+          config
+        );
+        if (data.status !== 'success')
+          throw new functions.https.HttpsError(
+            'cancelled',
+            'couldnt generate recharge link'
+          );
+        const txData: Transaction = {
+          user_uid: context.auth.uid,
+          amount: parseInt(amount as any),
+          fulfilled: false,
+        };
+        await db.doc('transactions/' + tx_ref).create(txData);
+        return data;
+      } catch (e: any) {
+        if (e! instanceof functions.https.HttpsError)
+          throw new functions.https.HttpsError('unknown', e.message);
+        throw e;
+      }
     }
-  }
-);
+  );
 export default generateRechargeLink;
