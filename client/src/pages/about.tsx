56141f41d98fee81c68cdfe4cd4d65c669370f96
michealnaita <michealnaita@gmail.com>
2023-02-22 12:20:12 +0300

    feat:changes the site details, adds how to on about page and deploy to new url

--- client/src/pages/about.tsx
+++ client/src/pages/about.tsx
@@ -8,11 +8,13 @@ import { Link } from 'react-router-dom';
 export default function AboutPage() {
   return (
     <Layout>
-      <div className="flex  h-full flex-col justify-center space-y-8 ">
-        <h1 className="text-3xl font-bold text-center">Kanji App</h1>
+      <div className="flex flex-col justify-center space-y-8 p-8">
+        <h1 className="text-2xl font-bold text-center">
+          Welcome to littleneck
+        </h1>
         <p className="text-skin-secondary text-2xl">
           Get connected to people who you can share your netflix or spotify bill
-          with
+          with.
         </p>
         <div className="text-xl space-y-2">
           <p className="font-semibold text">Sharing your bill means that;</p>
@@ -30,13 +32,32 @@ export default function AboutPage() {
               <img src={netflix} alt="" className="inline" />
             </span>{' '}
             premium for as low as{'  '}
-            <span className="text-skin-orange">shs.14000</span> for all your
+            <span className="text-skin-orange">shs.11000</span> for all your
             devices.
           </p>
           <p className="text-skin-secondary">
             <span className="text-skin-orange">&#x2022;</span> And to top it up,{' '}
             <span className="text-skin-orange">no credit card needed</span>, pay
             using mobile money.
+          </p>
+        </div>
+        <div className="text-xl space-y-2">
+          <p className="font-semibold text">All you need to do is...</p>
+          <p className="text-skin-secondary ">
+            <span className="text-skin-orange font-semibold">1.</span> Create an
+            Account
+          </p>
+          <p className="text-skin-secondary">
+            <span className="text-skin-orange font-semibold">2.</span> Load some
+            money onto your account via mobile money
+          </p>
+          <p className="text-skin-secondary">
+            <span className="text-skin-orange font-semibold">3.</span> Find and
+            join a household
+          </p>
+          <p className="text-skin-secondary">
+            <span className="text-skin-orange font-semibold">4.</span> And just
+            wait for the logins
           </p>
         </div>
         <Link to="/signin" className="text-center">
